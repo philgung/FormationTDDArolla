0eff6d120fe3a3969e8d7cef4d59cc8cdf89e37c
Philippe Gung <philippe.gung@youscribe.com>
2022-07-27 11:00:28 +0200

    fin du kata

--- LeapYearTests/UnitTest1.cs
+++ LeapYearTests/UnitTest1.cs
@@ -4,16 +4,6 @@ namespace LeapYearTests;
 
 public class Tests
 {
-    // Leap Year
-    // Write a function that returns true or false depending on whether
-    // its input integer is a leap year or not
-    // - A leap year is divisible by 4, but is not otherwise divisible
-    // by 100
-    // 1900 -> Atypical Common Year
-    // - Unless it is also divisible by 400
-    // 2000 -> Atypical Leap Year
-    
-
     [TestCase(2001)]
     [TestCase(1900)]
     public void A_typical_common_year(int year)
@@ -35,10 +25,21 @@ public class Tests
 
     private bool LeapYear(int year)
     {
-        if (year == 2001 || year == 1900)
-        {
-            return false;
-        }
-        return true;
+        return (year.NEstPasDivisiblePar(100) && year.EstDivisiblePar(4)) || 
+               (year.EstDivisiblePar(100) && year.EstDivisiblePar(400));
     }
+
+}
+
+public static class YearExtensions
+{
+    public static bool EstDivisiblePar(this int year, int valeur)
+    {
+        return year % valeur == 0;
+    }
+    public static bool NEstPasDivisiblePar(this int year, int valeur)
+    {
+        return year % valeur != 0;
+    }
+
 }
